b0c9c15a47d01715a8e2302fcfc02d311a7aab62
annn <annq@hivetect.vn>
2023-07-20 15:14:17 +0700

    INE4-65: update giao dien

--- src/main/java/com/hive/ycbm/models/BookingPage.java
+++ src/main/java/com/hive/ycbm/models/BookingPage.java
@@ -18,18 +18,12 @@ public class BookingPage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "page_id")
     private Long pageId;
-
     private String title;
-
-    @Column(unique = true)
     private  String bookingLink;
-
     private String intro;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "calendar_id")
     private Calendar calendar;
